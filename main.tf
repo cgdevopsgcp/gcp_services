81788f0747f4cc6fdf760e7c0a20bd808f8cd75a
cgdevopsgcp <97515980+cgdevopsgcp@users.noreply.github.com>
2022-02-18 17:52:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_compute_instance" "mastr" {
-  name         = "slavee1"
+resource "google_compute_instance" "auto-expire" {
+  name         = "servicevm"
   machine_type = "e2-medium"
   zone         = "asia-south1-c"
 
