9496ce03d8d7db54746f4cb529dfbc652266a56b
cgdevopsgcp <97515980+cgdevopsgcp@users.noreply.github.com>
2022-02-18 16:17:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_compute_instance" "dpais" {
-  name         = "adnmp"
+resource "google_compute_instance" "dpaisrnu" {
+  name         = "adnmp123"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
